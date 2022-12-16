429e2516aca85cc29bcbea4be9d23eab5b43999d
sevvalseray <sevvalseray@hotmail.com>
2022-12-16 14:52:33 +0300

    post assets was added

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import { StyleSheet } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
 import { NavigationContainer } from '@react-navigation/native';
 import Screens from './Screens';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function App() {
   return (
-    <SafeAreaView style={{flex :1}}>
+    <SafeAreaView style={{ flex: 1 }}>
+      <StatusBar style="dark" />
       <NavigationContainer>
         <Screens />
       </NavigationContainer>
--- components/shared/Post.js
+++ components/shared/Post.js
@@ -1,5 +1,6 @@
+import ReadMore from "@fawazahmed/react-native-read-more";
 import { StyleSheet, View, Text, Image, TouchableOpacity } from "react-native";
-import { More } from "../../Icons";
+import { BookMark, Comment, Heart, More, Share } from "../../Icons";
 import FitImage from "./FitImage";
 
 function Post({ post }) {
@@ -15,11 +16,41 @@ function Post({ post }) {
                     />
                     <Text style={styles.text}>{post.user.name}</Text>
                 </View>
-                <TouchableOpacity style={{alignItems: 'center', justifyContent: 'center'}} activeOpacity={0.5}>
+                <TouchableOpacity style={{ alignItems: 'center', justifyContent: 'center' }} activeOpacity={0.5}>
                     <More size={15} />
                 </TouchableOpacity>
             </View>
-            <FitImage src={post.image}/>
+            <FitImage src={post.image} />
+            <View style={styles.content}>
+                <View style={styles.actions}>
+                    <View style={styles.leftActions}>
+                        <TouchableOpacity style={{ marginRight: 14 }} activeOpacity={0.5}>
+                            <Heart size={24} fill={"black"} />
+                        </TouchableOpacity>
+                        <TouchableOpacity style={{ marginRight: 14 }} activeOpacity={0.5}>
+                            <Comment size={24} />
+                        </TouchableOpacity>
+                        <TouchableOpacity style={{ marginRight: 14 }} activeOpacity={0.5}>
+                            <Share size={24} />
+                        </TouchableOpacity>
+                    </View>
+                    <BookMark size={24} />
+                </View>
+                <View>
+                    <Text style={styles.likes}>{post.likes} Likes</Text>
+                </View>
+                <ReadMore numberOfLines={2}
+                    seeMoreStyle={{
+                        color: '#999'
+                    }}
+                    expandOnly={true}
+                    seeMoreText="more">
+                    <Text style={{ fontWeight: "600" }}>{post.user.name}</Text>
+                    {' '}
+                    {post.description}
+                </ReadMore>
+            </View>
+
         </View>
     )
 }
@@ -46,8 +77,23 @@ const styles = StyleSheet.create({
         flexDirection: 'row',
         alignItems: 'center'
     },
-    text:{
+    text: {
         fontSize: 14,
         fontWeight: '700'
+    },
+    actions: {
+        flexDirection: "row",
+        justifyContent: "space-between",
+        alignItems: 'center',
+        height: 40,
+    },
+    leftActions: {
+        flexDirection: "row",
+    },
+    content: {
+        paddingHorizontal: 15,
+    },
+    likes: {
+        fontWeight: "600"
     }
 })
